557d5abb3d1eaa99688cc1032fef77a75d0ca7cf
antoniocristovam <antoniocristovam@outlook.com.br>
2024-10-27 10:37:01 -0300

    refactor: Add PrismaAdapter for authentication
    This commit adds a new file, `prisma-adapter.ts`, which contains the implementation of the `PrismaAdapter` function for authentication. The `PrismaAdapter` function is responsible for handling user-related operations such as creating users, retrieving users by ID or email, updating user information, linking user accounts, creating sessions, and updating sessions.
    
    The `PrismaAdapter` function uses the Prisma ORM to interact with the database and perform the necessary operations. This commit also updates the authentication configuration in the `nextauth.api.ts` file to use the `PrismaAdapter` as the adapter for NextAuth.
    
    This refactor improves the code structure and separates the authentication logic into a dedicated adapter file, making it easier to maintain and extend in the future.

---
+++ src/lib/auth/prisma-adapter.ts
@@ -0,0 +1,178 @@
+import { Adapter } from "next-auth/adapters";
+import { prisma } from "../prisma";
+
+interface Account {
+  userId: string;
+  type: string;
+  provider: string;
+  providerAccountId: string;
+  refresh_token?: string;
+  access_token?: string;
+  expires_at?: number;
+  token_type?: string;
+  scope?: string;
+  id_token?: string;
+  session_state?: string;
+}
+
+export function PrismaAdapter(): Adapter {
+  return {
+    async createUser(user) {},
+
+    async getUser(id) {
+      const user = await prisma.user.findUniqueOrThrow({
+        where: {
+          id,
+        },
+      });
+
+      return {
+        id: user?.id,
+        name: user?.name,
+        email: user.email!,
+        username: user?.username,
+        avatar_url: user.avatar_url!,
+        emailVerified: null,
+      };
+    },
+
+    async getUserByEmail(email) {
+      const user = await prisma.user.findUniqueOrThrow({
+        where: {
+          email,
+        },
+      });
+
+      return {
+        id: user?.id,
+        name: user?.name,
+        email: user.email!,
+        username: user?.username,
+        avatar_url: user.avatar_url!,
+        emailVerified: null,
+      };
+    },
+
+    async getUserByAccount({ providerAccountId, provider }) {
+      const { user } = await prisma.account.findUniqueOrThrow({
+        where: {
+          provider_provider_account_id: {
+            provider: provider,
+            provider_account_id: providerAccountId,
+          },
+        },
+        include: {
+          user: true,
+        },
+      });
+      return {
+        id: user?.id,
+        name: user?.name,
+        email: user.email!,
+        username: user?.username,
+        avatar_url: user.avatar_url!,
+        emailVerified: null,
+      };
+    },
+
+    async updateUser(user) {
+      const prismaUser = await prisma.user.update({
+        where: {
+          id: user.id!,
+        },
+        data: {
+          name: user.name,
+          email: user.email,
+          avatar_url: user.avatar_url,
+        },
+      });
+
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        email: prismaUser.email!,
+        username: prismaUser?.username,
+        avatar_url: prismaUser.avatar_url!,
+        emailVerified: null,
+      };
+    },
+
+    async linkAccount(account: Account) {
+      await prisma.account.create({
+        data: {
+          user_id: account.userId,
+          type: account.type,
+          provider: account.provider,
+          provider_account_id: account.providerAccountId,
+          refresh_token: account.refresh_token,
+          access_token: account.access_token,
+          expires_at: account.expires_at,
+          token_type: account.token_type,
+          scope: account.scope,
+          id_token: account.id_token,
+          session_state: account.session_state,
+        },
+      });
+    },
+
+    async createSession({ sessionToken, userId, expires }) {
+      await prisma.session.create({
+        data: {
+          session_token: sessionToken,
+          user_id: userId,
+          expires: expires,
+        },
+      });
+
+      return {
+        sessionToken,
+        userId,
+        expires,
+      };
+    },
+
+    async getSessionAndUser(sessionToken) {
+      const { user, ...session } = await prisma.session.findUniqueOrThrow({
+        where: {
+          session_token: sessionToken,
+        },
+        include: {
+          user: true,
+        },
+      });
+      return {
+        session: {
+          expires: session.expires,
+          sessionToken: session.session_token,
+          userId: session.user_id,
+        },
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email!,
+          username: user.username,
+          avatar_url: user.avatar_url!,
+          emailVerified: null,
+        },
+      };
+    },
+
+    async updateSession({ sessionToken, expires, userId }) {
+      const prismaSession = await prisma.session.update({
+        where: {
+          session_token: sessionToken,
+        },
+        data: {
+          expires,
+          user_id: userId,
+        },
+      });
+
+      return {
+        expires: prismaSession.expires,
+        sessionToken: prismaSession.session_token,
+        userId: prismaSession.user_id,
+      };
+    },
+  };
+}
